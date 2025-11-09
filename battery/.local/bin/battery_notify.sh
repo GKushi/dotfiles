06807762d55407083ca0f2e1e54fb129fc7c6967
GKushi <gaworski.kasper@gmail.com>
2025-11-09 14:07:05 +0100

    battery script update & update deps

--- battery/.local/bin/battery_notify.sh
+++ battery/.local/bin/battery_notify.sh
@@ -3,6 +3,8 @@
 BATTERY_PATH="/sys/class/power_supply/BAT0"
 THRESHOLD=20
 CACHE_FILE="/tmp/.battery_notified"
+CRITICAL_THRESHOLD=5
+CRITICAL_CACHE_FILE="/tmp/.crititcal_battery_notified"
 
 # Get current battery percentage
 if [ ! -d "$BATTERY_PATH" ]; then
@@ -13,13 +15,24 @@ fi
 capacity=$(cat "$BATTERY_PATH/capacity")
 # status=$(cat "$BATTERY_PATH/status")
 
-# Only warn if discharging and below threshold
+# Low -> normal notification
 if [ "$capacity" -le "$THRESHOLD" ]; then
   if [ ! -f "$CACHE_FILE" ]; then
-    notify-send -u critical "Battery low" "Battery at ${capacity}%"
+    notify-send -u normal "Battery low" "Battery at ${capacity}%"
     touch "$CACHE_FILE"
   fi
 else
   # Reset cache if above threshold
   [ -f "$CACHE_FILE" ] && rm "$CACHE_FILE"
+fi
+
+# Critical -> critical notification
+if [ "$capacity" -le "$CRITICAL_THRESHOLD" ]; then
+  if [ ! -f "$CRITICAL_CACHE_FILE" ]; then
+    notify-send -u critical "Battery very low" "Battery at ${capacity}%"
+    touch "$CRITICAL_CACHE_FILE"
+  fi
+else
+  # Reset cache if above threshold
+  [ -f "$CRITICAL_CACHE_FILE" ] && rm "$CRITICAL_CACHE_FILE"
 fi
